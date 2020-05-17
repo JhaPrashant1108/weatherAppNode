33a850bd7173bf4b4452e57b62b2ce2917598001
JhaPrashant1108 <prashant.jha0008@gmail.com>
2020-05-17 14:46:59 +0530

    Second commit

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const geocode = require('./utils/geoCode')
 const weather = require('./utils/weather')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 
 const pathDirectory = path.join(__dirname,'..','public')
 const templateDirectory = path.join(__dirname,'..','templates','views')
@@ -77,6 +79,6 @@ app.get('*',(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
-    console.log('server is up and running')
+app.listen(port,()=>{
+    console.log('server is up and running at '+ port)
 })
